7df064258e2b2190005000e70010fc38cfd883c4
Paul Breton <paul.ravezb@gmail.com>
2017-08-10 19:29:35 +0200

    Push of the rework

---
+++ background.gd
@@ -0,0 +1,12 @@
+extends TextureFrame
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Initalization here
+	pass
+
+func _on_goto_scene_pressed():
+	get_node("/root/global").goto_scene("res://Level.tscn")
